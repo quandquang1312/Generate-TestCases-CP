dd304199ab700e41caaea6e1703a081feb7fa9b8
Quang Quan <91424630+quandquang1312@users.noreply.github.com>
2023-04-25 16:46:45 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -10,7 +10,7 @@ typedef unsigned long long ull;
 #define FOR(i,a,b) for (int i=a; i<b; i++) 
 #define LOG(x) cout << x << "\n";
 
-int t, n, ti, k;
+int t = 1, n, ti, k;
 ll a[MAXN], b[MAXN];
 vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(k+1,vector<int>(3,0)));
 string str;
@@ -30,11 +30,11 @@ int main()
 		freopen("rs.out", "w", stdout);
     #endif
     
-    t = 1;
-	bool cintest = true;
-    if (cintest)
-    	cin >> t;
-    
+	t = 1;
+   	#ifdef LOCAL
+		cin >> t;
+    #endif
+	
 	while (t--)
         solve();
     return 0;
