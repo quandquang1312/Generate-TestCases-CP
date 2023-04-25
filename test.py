c163a73b949d0eabec7470c5cde75d1e4b72b227
Quang Quan <91424630+quandquang1312@users.noreply.github.com>
2023-04-25 19:42:31 +0700

    Update test.py

--- test.py
+++ test.py
@@ -14,11 +14,12 @@ sz = len(out)
 for idx in range(sz):
     correct = True
     for ele1, ele2 in zip(out[idx], rs[idx]):
-        if (ele1 != ele2):
+        if (ele1.lower() != ele2.lower()):
             correct = False
             break
     print("Test " + str(idx+1) + ": ", end='')
     if (correct):
-        print('Passed')
+        print('\033[1;32m Passed\033[0;0m')
     else:
-        print('Failed')
+        print('\033[1;31m Failed\033[0;0m')
+        print('         \033[1;33mAnswer is: \033[0;0m\033[1;32m' + ele1 + '\033[0;0m\033[1;33m, but found: \033[0;0m\033[1;31m' + ele2 + '\033[0;0m')
