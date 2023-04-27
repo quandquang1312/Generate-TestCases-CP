c448d3ca50754035dd98948f2d122093f4246bfd
Quang Quan <91424630+quandquang1312@users.noreply.github.com>
2023-04-27 09:33:12 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import sys
+
 def read_from_file(name):
     rs = []
     with open(str(name) + '.out', 'r') as f:
@@ -7,19 +9,34 @@ def read_from_file(name):
             rs.append(tmp)
     return rs
 
-out = read_from_file('out')
-rs = read_from_file('rs')
+def get_no_tc():
+    with open('in.in', 'r') as f:
+        tc = f.readline()
+    return int(tc)
 
-sz = len(out)
-for idx in range(sz):
-    correct = True
-    for ele1, ele2 in zip(out[idx], rs[idx]):
-        if (ele1.lower() != ele2.lower()):
-            correct = False
-            break
-    print("Test " + str(idx+1) + ": ", end='')
-    if (correct):
-        print('\033[1;32m Passed\033[0;0m')
+if __name__ == '__main__':
+    tc = get_no_tc()
+    out = read_from_file('out')
+    rs = read_from_file('rs')
+
+    if (len(sys.argv) > 1 and sys.argv[1] == 'D'):
+        print('\033[1;31mDebug is on\033[0;0m')
+        for elements in out:
+            for element in elements:
+                print(element, end=' ')  
+            print()
     else:
-        print('\033[1;31m Failed\033[0;0m')
-        print('         \033[1;33mAnswer is: \033[0;0m\033[1;32m' + ele1 + '\033[0;0m\033[1;33m, but found: \033[0;0m\033[1;31m' + ele2 + '\033[0;0m')
+        for idx in range(tc):
+            correct = True
+            no_line = len(out)//tc
+            for line in range(no_line):
+                for ele1, ele2 in zip(out[idx+line], rs[idx+line]):
+                    if (ele1.lower() != ele2.lower()):
+                        correct = False
+                        break
+                print("Test " + str(idx+1) + ": ", end='')
+                if (correct):
+                    print('\033[1;32m Passed\033[0;0m')
+                else:
+                    print('\033[1;31m Failed\033[0;0m')
+                    print('         \033[1;33mAnswer is: \033[0;0m\033[1;32m' + ele1 + '\033[0;0m\033[1;33m, but found: \033[0;0m\033[1;31m' + ele2 + '\033[0;0m')
